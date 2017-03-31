4522e3ee936dccad7930a2a0e710c79ae164405c
Gabriel Martinez <gabriel.martinez.2@asu.edu>
2017-03-30 17:00:19 -0700

    Working on button check feature.

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
-var express = require('express')
-var app = express()
+// var express = require('express')
+// var app = express()
+
+var request = require('request')
+var cheerio = require('cheerio')
+var Case = require('case')
+
+var url = process.argv[2];
 
 // GET method route
-app.get('/', function (req, res) {
-  res.send('GET request to the homepage')
-})
+// app.get('/', function (req, res) {
+//   res.send('GET request to the homepage')
+// })
 
 // POST method route
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
-})
+// app.post('/', function (req, res) {
+//   res.send('POST request to the homepage')
+// })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
+
+//testing url argument site buttons
+request(url, function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html)
+    $('.btn').each(function(i, element){
+      var buttonTexts = $(this);
+      if (Case.of(buttonTexts.text()) == "sentence"){
+        var pf = "PASS"
+      } else if (Case.of(buttonTexts.text()) == "header") {
+        var pf = "PASS"
+      } else {
+        var pf = "FAIL"
+      }
+      process.stdout.write("Button Text: " + buttonTexts.text().trim() + " / Casing Style: " + Case.of(buttonTexts.text()) + " / PASS-FAIL: " + pf + "\n");
+      // console.log(buttonTexts.text());
+      // console.log(Case.of(buttonTexts.text()));
+    });
+  }
 })
---
+++ test/buttons.js
@@ -0,0 +1,13 @@
+describe("Sentence Casing Check", function() {
+
+  it('should have sentence casing', function() {
+    // Test implementation goes here
+  });
+
+  describe("RGB to Hex conversion", function() {
+  it("converts the basic colors", function() {
+
+  });
+});
+
+});
