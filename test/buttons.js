1d1805497302aa7913e905eb9e2e9b548957a090
Gabriel Martinez <gabriel.martinez.2@asu.edu>
2017-04-04 16:50:05 -0700

    Adding more to button check.

--- test/buttons.js
+++ test/buttons.js
@@ -1,9 +1,42 @@
-log = console.log
-expect = require("chai").expect
-should = require("chai").should()
-_ = require("lodash")
+// var express = require('express')
+// var app = express()
 
-describe("#mocha basics", ())
+var request = require('request')
+var cheerio = require('cheerio')
+var Case = require('case')
 
-// var mocha = require("mocha")
-// var chai = ("chai");
+var url = process.argv[2];
+
+// GET method route
+// app.get('/', function (req, res) {
+//   res.send('GET request to the homepage')
+// })
+
+// POST method route
+// app.post('/', function (req, res) {
+//   res.send('POST request to the homepage')
+// })
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
+
+//testing url argument site buttons
+request(url, function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html)
+    $('.btn').each(function(i, element){
+      var buttonTexts = $(this);
+      if (Case.of(buttonTexts.text()) == "sentence"){
+        var pf = "PASS"
+      } else if (Case.of(buttonTexts.text()) == "header") {
+        var pf = "PASS"
+      } else {
+        var pf = "FAIL"
+      }
+      process.stdout.write("Button Text: " + buttonTexts.text().trim() + " / Casing Style: " + Case.of(buttonTexts.text()) + " / PASS-FAIL: " + pf + "\n");
+      // console.log(buttonTexts.text());
+      // console.log(Case.of(buttonTexts.text()));
+    });
+  }
+})
