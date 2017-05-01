a726dc44db161cde91f6530686c819c23a156080
Gabriel Martinez <gibbi33@gmail.com>
2017-04-30 23:07:11 -0700

    Working on delay load of favicon results.

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/baselineImages", express.static(__dirname + '/tests/baselineImages'));
+app.use("/downloadedImages", express.static(__dirname + '/tests/downloadedImages'));
+app.use("/resultImages", express.static(__dirname + '/tests/resultImages'));
 
 app.use('/', index);
 app.use('/buttons', buttons);
