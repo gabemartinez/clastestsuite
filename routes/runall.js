4eb0e08320c7564773335350d0e37bdf1256b3f8
Gabriel Martinez <gabriel.martinez.2@asu.edu>
2017-05-19 16:45:40 -0700

    Export to pdf

--- routes/runall.js
+++ routes/runall.js
@@ -3,6 +3,9 @@ var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
 
+var fs = require('fs');
+var pdf = require('html-pdf');
+
 // var rmdir = require('rmdir');
 // var path = './tests/';
 
@@ -31,25 +34,40 @@ router.get('/', function(req, res, next) {
   //   console.log(files);
   //   console.log('all files are removed');
   // });
-  res.render('../views/pages/runall', { title: 'Run All Test' });
+  res.render('../views/pages/runall', { title: 'Run All Tests' });
 });
 
-router.get('/singlegrade/:linkid', function(req, res, next) {
+router.get('/singlegrade/savepdf', function(req, res, next) {
+  // res.render('../views/pages/singlegrade', { title: 'Single Page Grade', linkid, data });
+  res.json(req.hostname)
+  var html = fs.readFileSync('./views/pages/test.html', 'utf8');
+  var options = { format: 'Letter' };
+
+  pdf.create(html, options).toFile('./pdfReports/test.pdf', function(err, res) {
+    if (err) return console.log(err);
+    console.log(res);
+  });
+});
+
+router.get('/singlegrade/:dataid/:linkid', function(req, res, next) {
   // console.log(req.params);
   // res.json(req.params);
+  var dataid = req.params.dataid;
   var linkid = req.params.linkid;
-  res.render('../views/pages/singlegrade', { title: 'Run asfasdf', linkid: linkid });
+  var data = require('../somedata/'+dataid+'.json');
+  res.render('../views/pages/singlegrade', { title: 'Single Page Grade', linkid, data });
 });
 
 // POST /runall gets urlencoded bodies
 router.post('/', urlencodedParser, function (req, res, next) {
   var url = req.body.page;
   // console.log(url);
-  var data = require('../somedata/'+url);
+  var data = require('../somedata/'+url+'.json');
+  // var datanourl = req.body.page;
   // var sitemapLinks = req.sitemapLinks;
 
   //res.render('../views/pages/runall-success', { title: 'Run All Test', page: page, results: results, un: un });
-  res.render('../views/pages/runall-success', { title: 'Run All Test', url: url, data });
+  res.render('../views/pages/runall-success', { title: 'Site Test Report', url: url, data, url });
 });
 
 module.exports = router;
