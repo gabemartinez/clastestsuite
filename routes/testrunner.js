174eea4ad365592bf9955766f988f4cd04efabc9
Gabriel Martinez <gabriel.martinez.2@asu.edu>
2017-05-30 16:27:33 -0700

    Defined site schema, integrated mongoose.

--- routes/testrunner.js
+++ routes/testrunner.js
@@ -3,6 +3,10 @@ var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://clastest:blah33@ds143141.mlab.com:43141/clastestsuite');
+var Site = require('../models/Site');
+
 var websparkcheck = require('../middleware/websparkcheck');
 
 // create application/json parser
@@ -17,8 +21,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', urlencodedParser, websparkcheck, function(req, res, next) {
+
   var site = req.body.site;
-  res.render('../views/pages/testrunner-started', { site });
+
+  var thisSite = new Site({ url: site, links: ['blah1', 'blah2', 'blah3', 'blah4'] });
+  thisSite.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(thisSite);
+    }
+  });
+
+  res.render('../views/pages/testrunner-started', { site, thisId: thisSite._id });
 });
 
 router.get('/notwebspark', function(req, res, next) {
